60eb70a85b264cce2447827e3696f06d505e3266
Charles <charlesdpulliam@gmail.com>
2020-01-15 19:04:48 -0600

    css imported

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import shoppingList from './scripts/shopping-list';
 import $ from 'jquery';
+import './index.css';
 const main = function () {
   shoppingList.bindEventListeners();
   shoppingList.render();
